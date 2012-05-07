3e79debc1bdb4746690ca17655681957ad547e6f
Stefan Krause-Kjaer <stefan.krausekjaer@gmail.com>
2012-05-07 11:39:21 +0200

    Empty Project committed.

---
+++ pongzor/src/com/iha/pongzor/Menu.java
@@ -0,0 +1,13 @@
+package com.iha.pongzor;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Menu extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
