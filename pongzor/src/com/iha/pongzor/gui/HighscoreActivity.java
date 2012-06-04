3c592212678dc7e26dc175e099273e77acda65ca
Stefan Krause-Kjaer <stefan.krausekjaer@gmail.com>
2012-06-04 14:45:59 +0200

    Changed projekt encoding to UTF8 and made the highscore mockup work.

--- pongzor/src/com/iha/pongzor/gui/HighscoreActivity.java
+++ pongzor/src/com/iha/pongzor/gui/HighscoreActivity.java
@@ -5,13 +5,13 @@ import com.iha.pongzor.data.*;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
-import java.util.Map;
 
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -31,7 +31,9 @@ public class HighscoreActivity extends Activity {
 
 		
 		setContentView(R.layout.highscore);
-		List<HighScore> hsList = createDummyData();
+		List<HighScore> hsList = createDummyData(null);
+		hsList = createDummyData(hsList);
+		hsList = createDummyData(hsList);
 		ListView lv = (ListView) findViewById(R.id.highscore_list);
 
 		lv.setAdapter(createAdapter(hsList));
@@ -40,20 +42,20 @@ public class HighscoreActivity extends Activity {
 	private ListAdapter createAdapter(List<HighScore> hsList) {
 
 		ArrayAdapter<HighScore> arryAdapter = new ArrayAdapter<HighScore>(this,
-				0) {
+				R.layout.highscore_list_field, hsList) {
 
 			@Override
 			public View getView(int position, View convertView, ViewGroup parent) {
 
 				HighScore hs = this.getItem(position);
 
-				View v = getLayoutInflater().inflate(
-						R.layout.highscore_list_field, null);
-				TextView twName = (TextView) findViewById(R.id.HighScore_playerName);
-				TextView twScore = (TextView) findViewById(R.id.HighScore_playerScore);
+				LinearLayout v = (LinearLayout) getLayoutInflater().inflate(R.layout.highscore_list_field, null);
+				
+				TextView twName = (TextView) v.findViewById(R.id.HighScore_playerName);
+				TextView twScore = (TextView) v.findViewById(R.id.HighScore_playerScore);
 
 				twName.setText(hs.getName());
-				twScore.setText(hs.getScore());
+				twScore.setText(String.valueOf(hs.getScore()));
 				return v;
 			}
 		};
@@ -61,15 +63,16 @@ public class HighscoreActivity extends Activity {
 		return arryAdapter;
 	}
 
-	private List<HighScore> createDummyData() {
-		ArrayList<HighScore> hsList = new ArrayList<HighScore>();
+	private List<HighScore> createDummyData(List<HighScore> hsList) {
+		if(hsList == null)
+			hsList = new ArrayList<HighScore>();
 
 		Date d = new Date(2012, 5, 31);
 		hsList.add(new HighScore("Hans", 6000, 420, d));
 		hsList.add(new HighScore("Lars", 9999, 130, d));
 		hsList.add(new HighScore("Linux Thorvaldsen", 1337, 1, d));
 		hsList.add(new HighScore("Signe", 2156, 180, d));
-		hsList.add(new HighScore("B�rge", 88888, 580, d));
+		hsList.add(new HighScore("Børge", 88888, 580, d));
 		hsList.add(new HighScore("Tina", 123456, 730, d));
 		hsList.add(new HighScore("Anette", 1, 10, d));
 		hsList.add(new HighScore("Frans", 89, 20, d));
